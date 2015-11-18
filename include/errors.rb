24c4ad8706ef2a6216f90d3d9efa27494f249151
kaspernj <k@spernj.org>
2015-11-18 20:38:11 +0100

    Began working on implementing better errors for logging

--- include/errors.rb
+++ include/errors.rb
@@ -1,3 +1,14 @@
 class ApsisOnSteroids::Errors
+  class HttpError < RuntimeError
+    attr_accessor :http_response
+
+    def self.http_error_from_response(args)
+      error = ApsisOnSteroids::Errors::HttpError.new("Unexpected result: '#{res["Result"]}'.")
+      error.http_response = args.fetch(:response)
+
+      raise error
+    end
+  end
+
   class SubscriberNotFound < RuntimeError; end
 end
