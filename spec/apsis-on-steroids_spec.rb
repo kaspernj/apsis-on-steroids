b1370960f30509f4fb3b13dc9d32170b8684430b
nj <nj@downtown.dk>
2015-11-26 20:38:43 +0100

    Support API-key by environment variable

--- spec/apsis-on-steroids_spec.rb
+++ spec/apsis-on-steroids_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "ApsisOnSteroids" do
   let(:aos) do
     ApsisOnSteroids.new(
-      api_key: File.read("#{File.dirname(__FILE__)}/api_key.txt").strip,
+      api_key: ENV['APSIS_API_KEY'] || File.read("#{File.dirname(__FILE__)}/api_key.txt").strip,
       debug: false
     )
   end
